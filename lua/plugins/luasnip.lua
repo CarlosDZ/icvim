ae23c0366764485f0530868820d9a63bd2695fc5
CarlosDZ <carlos.dz16180@gmail.com>
2025-12-04 10:39:32 +0100

    Snippet: HTML5

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -2,6 +2,9 @@ return {
   "L3MON4D3/LuaSnip",
   config = function()
     require("luasnip.loaders.from_vscode").lazy_load()
+    require("luasnip.loaders.from_lua").load({
+        paths = "~/.config/nvim/lua/snippets"
+    })
   end,
 }
 
